d42d0bf51ac0fd3caca7f785fe376bbf8bdaff43
Msteven <msdavid55@gmail.com>
2024-02-27 22:54:06 -0600

    initialized styling changes

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Projects() {
   return (
     <div>
@@ -6,7 +8,7 @@ export default function Projects() {
           <div className="p-2 w-auto">
             <h3 className="row m-2 justify-content-center align-items-center">Spotlight Project!</h3>
             <img src="/translate app screenshot.png" id="firstProject"
-              className="card-img-top  border border-5 border-warning" alt="A screenshot of a translation app" />
+              className="card-img-top rounded-4 border border-5 border-warning" alt="A screenshot of a translation app" />
             <div className="card-body">
               <h5 className="card-title my-2">The DaTA (Daily Translation App)</h5>
               <p className="card-text">This was my first collaborative project where we coded a translation app using API
@@ -21,7 +23,7 @@ export default function Projects() {
         <h4 className="row m-2 justify-content-center align-items-center">Other Projects for Consideration!</h4>
 
         <div className="m-4 p-4 w-25">
-          <img src="/weather viewing app screenshot.png" height="150px" className="card-img-top" alt="..." />
+          <img src="/weather viewing app screenshot.png" height="150px" className="card-img-top rounded-3" alt="..." />
           <div className="card-body">
             <h5 className="card-title my-2">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
@@ -31,7 +33,7 @@ export default function Projects() {
         </div>
 
         <div className="m-4 p-4 w-25">
-          <img src="/timed coding quiz screenshot.png" height="150px" className="card-img-top" alt="..." />
+          <img src="/timed coding quiz screenshot.png" height="150px" className="card-img-top rounded-3" alt="..." />
           <div className="card-body">
             <h5 className="card-title my-2">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
@@ -41,7 +43,7 @@ export default function Projects() {
         </div>
 
         <div className="m-4 p-4 w-25" id="lastProject">
-          <img src="/README Generator Screenshot.png" height="150px" className="card-img-top" alt="..." />
+          <img src="/README Generator Screenshot.png" height="150px" className="card-img-top rounded-3" alt="..." />
           <div className="card-body">
             <h5 className="card-title my-2">README Generator</h5>
             <p className="card-text">A CLI application that utilizes back-end technologies such as Node.js to allow the user
