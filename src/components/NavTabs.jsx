6ccd9bf2be7bd7239e5fa51ccc084d44059df4aa
Michael Davidson <msdavid55@gmail.com>
2024-02-28 22:51:57 -0600

    fixed collapse functionality of nav bar

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,50 +1,61 @@
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-underline nav-fill d-flex justify-content-end">
-      <li className="nav-item fs-2 d-flex align-items-center justify-content-center">
-        Michael Davidson
-      </li>
-      
-      <li className="nav-item">
-        <a
-          href="#landing"
-          onClick={() => handlePageChange('Landing')}
-          className={currentPage === 'Landing' ? 'nav-link active text-light' : 'nav-link text-light'}
-        >
-          Landing
-        </a>
-        
-      </li>
-      <li className="nav-item">
-        <a
-          href="#projects"
-          onClick={() => handlePageChange('Projects')}
-          className={currentPage === 'Projects' ? 'nav-link active text-light' : 'nav-link text-light'}
-        >
-          Projects
-        </a>
-      </li>
+    <nav class="navbar nav-underline nav-fill navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <a class="fs-4 text-light navbar-brand" href="/">Michael Davidson</a>
 
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active text-light' : 'nav-link text-light'}
-        >
-          About
-        </a>
-      </li>
-      
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active text-light' : 'nav-link text-light'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="justify-content-end collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li className="nav-item">
+              <a
+                href="#landing"
+                onClick={() => handlePageChange('Landing')}
+                className={currentPage === 'Landing' ? 'nav-link active text-light' : 'nav-link text-light'}
+              >
+                Landing
+              </a>
+
+            </li>
+            <li className="nav-item">
+              <a
+                href="#projects"
+                onClick={() => handlePageChange('Projects')}
+                className={currentPage === 'Projects' ? 'nav-link active text-light' : 'nav-link text-light'}
+              >
+                Projects
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active text-light' : 'nav-link text-light'}
+              >
+                About
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active text-light' : 'nav-link text-light'}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
